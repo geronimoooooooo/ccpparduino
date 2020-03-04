0c7c2bbe9c10838ca7fd9a116376252f15a9a906
geronimoooooooo <geronimoooooooo@gmail.com>
2020-03-04 12:40:14 +0100

    Update print.cpp

--- print.cpp
+++ print.cpp
@@ -117,10 +117,15 @@ int bootCount = 2;
   snprintf(buffer, 75, "connecting to: %s", s2);
   printAddSimpleLineOfText(buffer);
 ##########################################
-  char s1[100], s2[100];	// two character arrays
-  int i, j;
-  printf ("Enter first string:\n");
-  fgets (s1, 100, stdin);	// Read string1 from stdin, of maximum length 100 
+char s1[100], s2[100],i;          //two character arrays s1,s2
+    printf("Enter string s1:\n");     
+    fgets(s1, 100, stdin);           // Read string from stdin, of maximum length 100 
+    for(i = 0; s1[i] != '\0'; ++i)   // copy to s2 
+    {
+        s2[i] = s1[i];
+    }
+    s2[i] = '\0';                   // Append NULL character at end to mark end of string
+    printf("String s2: %s", s2);
 ##########################################
 ##########################################
 ##########################################
