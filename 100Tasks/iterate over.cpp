ca8293cdf881804fbb27123a6e2807de132dce8e
geronimoooooooo <geronimoooooooo@gmail.com>
2020-03-06 16:29:22 +0100

    Update iterate over.cpp

--- 100Tasks/iterate over.cpp
+++ 100Tasks/iterate over.cpp
@@ -1,16 +1,20 @@
 1. char array
+2. int array
 
 
 
 
 
-
-1#############################
+1 #############################
 char myStr[] = "this is some string";
 for(byte i=0; i<sizeof(myStr)-1; i++)
   {Serial.println(myStr[i]);
  }
-#############################
+2 #############################
+int myVal[] = {12,34,56};
+for (byte i = 0; i < (sizeof(myVal) / sizeof(myVal[0])); i++) {
+  // do something with myVal[i]
+}
 #############################
 #############################
 #############################
