30907c5e6facf1e6034956a714d6721034edbd3c
geronimoooooooo <geronimoooooooo@gmail.com>
2020-03-17 15:28:20 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,4 +1,4 @@
-functions are generally call by value
+functions are generally call by value. arrays are passed as pointers to functions.
 ##################
 void swapTwoNumbers(int *a, int *b)
 {
@@ -6,6 +6,8 @@ void swapTwoNumbers(int *a, int *b)
     *a = *b;
     *b = temp;
 }
+
+swapTwoNumbers(&first, &second);
 /*
 int first = 10;
 int second = 20;
@@ -14,3 +16,19 @@ swapTwoNumbers(&first, &second);
 printf("first: %d\nsecond: %d\n", first, second);
 // values will be swapped
 ##########################
+// Size must be passed!
+// Otherwise, this function has no way of knowing how big the array is.
+void printIntArray(int *arr, size_t size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        printf("arr[%d] is: %d\n", i, arr[i]);
+    }
+}
+printIntArray(my_arr, size);
+/*
+int my_arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int size = 10;
+printIntArray(my_arr, size);
+// will print "arr[0] is: 1" etc
+*/
+####################################
