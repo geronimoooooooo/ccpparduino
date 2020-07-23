b6270cf5ad71654245c4f21f56982f0911af425e
geronimoooooooo <geronimoooooooo@gmail.com>
2020-07-23 15:19:46 +0200

    Update I2Cscan().cpp

--- I2Cscan().cpp
+++ I2Cscan().cpp
@@ -65,61 +65,56 @@ void loop()
 https://playground.arduino.cc/Main/I2cScanner/
 
 #include <Arduino.h>
-#include <M5Stack.h>
 #include <Wire.h>
 
-
-void setup()
-{
+void setup() {
   Wire.begin();
 
-  Serial.begin(115200);//9600
-  while (!Serial);             // Leonardo: wait for serial monitor
+  Serial.begin(115200);  // 9600
+  while (!Serial)
+    ;  // Leonardo: wait for serial monitor
   Serial.println("\nI2C Scanner");
 }
 
-
-void loop()
-{
+void loop() {
   byte error, address;
   int nDevices;
 
   Serial.println("Scanning...");
 
   nDevices = 0;
-  for(address = 1; address < 127; address++ ) 
-  {
+  for (address = 1; address < 127; address++) {
     // The i2c_scanner uses the return value of
     // the Write.endTransmisstion to see if
     // a device did acknowledge to the address.
     Wire.beginTransmission(address);
     error = Wire.endTransmission();
 
-    if (error == 0)
-    {
+    if (error == 0) {
       Serial.print("I2C device found at address 0x");
-      if (address<16) 
-        Serial.print("0");
-      Serial.print(address,HEX);
+      if (address < 16) Serial.print("0");
+      Serial.print(address, HEX);
       Serial.println("  !");
 
       nDevices++;
-    }
-    else if (error==4) 
-    {
+    } else if (error == 4) {
       Serial.print("Unknown error at address 0x");
-      if (address<16) 
-        Serial.print("0");
-      Serial.println(address,HEX);
-    }    
+      if (address < 16) Serial.print("0");
+      Serial.println(address, HEX);
+    }
   }
   if (nDevices == 0)
     Serial.println("No I2C devices found\n");
   else
     Serial.println("done\n");
 
-  delay(4000);           // wait 4 seconds for next scan
+  delay(4000);  // wait 4 seconds for next scan
 }
+
+Scanning...
+I2C device found at address 0x68  !
+I2C device found at address 0x76  !
+done
 ##################################################################
 void I2Cscan()
 {
