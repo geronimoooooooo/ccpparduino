e87b9e12c9843c99643bf88d7f671b377d3e3afc
geronimoooooooo <geronimoooooooo@gmail.com>
2020-08-12 20:27:43 +0200

    Create pin.cpp

---
+++ pin.cpp
@@ -0,0 +1,13 @@
+Auflistung einer anderer sinnvoller Funktionen: https://randomnerdtutorials.com/esp32-adc-analog-read-arduino-ide/
+
+const int pin = 35;
+
+pinMode(pin, mode);
+pinMode(pin, INPUT);
+pinMode(led, OUTPUT);
+
+analogRead(pin); digitalRead(pin);
+int val = analogRead(pin);
+
+pinMode(gpio,OUTPUT);
+digitalWrite(gpio, HIGH/LOW);
